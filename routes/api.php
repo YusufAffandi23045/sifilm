38aaa253e4a8b3f82f11c704945472fbe16d83fb
RafiDwiSaputra058 <149571526+RafiDwiSaputra058@users.noreply.github.com>
2025-06-10 19:25:50 +0700

    api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MovieApiController;
 use App\Http\Controllers\MovieAdminApiController;
+use App\Http\Controllers\GenreApiController;
 use App\Http\Controllers\AdminAuthController;
 
 
@@ -18,21 +19,25 @@ use App\Http\Controllers\AdminAuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/', [MovieController::class, 'index']);
+Route::get('/', [MovieApiController::class, 'index']);
 
 Route::post('/admin/register', [AdminAuthController::class, 'register']);
 Route::post('/admin/login', [AdminAuthController::class, 'loginApi']);
 
 Route::middleware('jwt.auth')->group(function () {
+
     Route::get('admin/', [MovieAdminApiController::class, 'index']);
-    Route::get('admin/{id}', [MovieAdminApiController::class, 'show']);
     Route::post('admin/', [MovieAdminApiController::class, 'store']);
     Route::put('admin/{id}', [MovieAdminApiController::class, 'update']);
     Route::delete('admin/{id}', [MovieAdminApiController::class, 'destroy']);
     Route::post('/admin/logout', [AdminAuthController::class, 'logoutApi']);
+
+    Route::get('admin/genre', [GenreApiController::class, 'index']);
+    Route::post('admin/genre', [GenreApiController::class, 'store']);
+    Route::put('admin/genre/{id}', [GenreApiController::class, 'update']);
+    Route::delete('admin/genre/{id}', [GenreApiController::class, 'destroy']);
+
+    Route::get('admin/{id}', [MovieAdminApiController::class, 'show']);
+    Route::get('admin/genre/{id}', [GenreApiController::class, 'show']);
 });
-Route::get('/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/{id}', [MovieApiController::class, 'show'])->name('movies.show');
